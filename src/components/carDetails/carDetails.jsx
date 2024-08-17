0ff7bb423f49848e34ce9b3601ce76b392f950c7
Dmitriy Khegay <dmitriy88k@gmail.com>
2024-08-17 12:06:59 -0400

    started working on car details page.

---
+++ src/components/carDetails/carDetails.jsx
@@ -0,0 +1,25 @@
+import styles from "./carDetails.module.css";
+
+const CarDetails = () => {
+  return (
+    <div>
+      <div className={styles.car_images}>
+
+      </div>
+      
+      <div className={styles.car_main_discription}>
+
+      </div>
+
+      <div className={styles.car_detail_discription}>
+
+
+      </div>
+
+      <div className={styles.car_location_map}></div>
+
+    </div>
+  )
+}
+
+export default CarDetails;
