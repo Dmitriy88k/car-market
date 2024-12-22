b7e5c38b28c8a0836ca015cac993d58b834637e9
Dmitriy Khegay <dmitriy88k@gmail.com>
2024-12-22 09:54:16 -0500

    New corousel was added to the project on the car details page

--- src/pages/carDetails/carDetails.jsx
+++ src/pages/carDetails/carDetails.jsx
@@ -3,18 +3,20 @@ import { useParams } from "react-router-dom";
 import { db } from "../../firebase";
 import { doc, getDoc } from "firebase/firestore";
 import styles from "../carDetails/carDetails.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Thumbs } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import Footer from "../../components/footer/footer"
+import Footer from "../../components/footer/footer";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import GasStationImg from "../../assets/gas-station.png"
+import CarType from "../../assets/car.png"
+import CarMileage from "../../assets/speed.png"
+import CarColor from "../../assets/drawing.png"
+import CarPrice from "../../assets/price-tag.png"
+import Drivetrain from "../../assets/drivetrain.png"
+import { capitalize } from "../../components/hooks/useCapitalize"
 
 const CarDetails = () => {
   const { id } = useParams();
   const [car, setCar] = useState(null);
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
     const fetchCarDetails = async () => {
@@ -36,58 +38,81 @@ const CarDetails = () => {
   return (
     <div className={styles.car_details_page}>
       <div className={styles.car_details_section}>
-        <Swiper
-          modules={[Navigation, Thumbs]}
-          navigation
-          loop={true}
-          pagination={{ clickable: true }}
-          onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
-          thumbs={{ swiper: thumbsSwiper }}
-          className={styles.main_car_image_swiper}
+        < Carousel
+          infiniteLoop
+          className={styles.caroussel_section}
+          showThumbs
         >
           {car.images.map((image, index) => (
-            <SwiperSlide key={index}>
-              <img
-                src={image}
-                alt={`${car.make} ${car.model} ${index + 1}`}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        <Swiper
-          onSwiper={setThumbsSwiper}
-          slidesPerView={10}
-          spaceBetween={40}
-          modules={[Thumbs]}
-          watchSlidesProgress={true}
-          watchSlidesVisibility={true}
-          loop={false}
-          centeredSlides={true}
-          className={styles.thumbnails_swiper}
-          breakpoints={{
-            
-            955: { slidesPerView: 3, spaceBetween: 10, },
-            1290: { slidesPerView: 4, spaceBetween: 10, },
-          }}
-        >
-          {car.images.map((image, index) => (
-            <SwiperSlide key={index} className={activeIndex === index ? "swiper-slide-thumb-active" : ""}>
+            <div key={index} className={styles.car_swiper}>
               <img
                 src={image}
                 alt={`Thumbnail ${index + 1}`}
-                className={styles.thumbnail_image}
-                
+                className={styles.main_image}
               />
-            </SwiperSlide>
+            </div>
           ))}
-        </Swiper>
+
+        </Carousel>
 
         <h1>
           {car.year} {car.make} {car.model}  
         </h1>
 
         <h2>${car.price.toLocaleString()}</h2>
+      </div>
+      <div className={styles.car_description_section}>
+          
+          <div className={styles.car_specifications}>
+            <img src={CarPrice} alt="" />
+            <div className={styles.car_specifications_text}>
+              <p className={styles.car_specifications_text_header}>Price</p>
+              <p>${car.price.toLocaleString()}</p>
+            </div>
+          </div>
+          
+          <div className={styles.car_specifications}>
+            <img src={CarMileage} alt="" />
+            <div className={styles.car_specifications_text}>
+              <p className={styles.car_specifications_text_header}>Mileage</p>
+              <p>{car.mileage.toLocaleString()}</p>
+            </div>
+          </div>
+
+          <div className={styles.car_specifications}>
+          <img src={CarColor} alt="" />
+            <div className={styles.car_specifications_text}>
+              <p className={styles.car_specifications_text_header}>Color</p>
+              <p>{capitalize(car.color)}</p>
+            </div>
+          </div>
+
+          
+          <div className={styles.car_specifications}>
+          <img src={GasStationImg} alt="" />
+            <div className={styles.car_specifications_text}>
+              <p className={styles.car_specifications_text_header}>Fuel Type</p>
+              <p>{capitalize(car.fuel)}</p>
+            </div>
+          </div>
+
+          <div className={styles.car_specifications}>
+            <img src={Drivetrain} alt="" />
+            <div className={styles.car_specifications_text}>
+              <p className={styles.car_specifications_text_header}>Drivetrain</p>
+              <p>{car.drivetrain}</p>
+            </div>
+          </div>
+
+          <div className={styles.car_specifications}>
+          <img src={CarType} alt="" style={{ width: "35px", height: "55px" }}/>
+            <div className={styles.car_specifications_text}>
+              <p className={styles.car_specifications_text_header}>Body Type</p>
+              <p>{capitalize(car.type)}</p>
+            </div>
+          </div>
+
+
       </div>
 
       <Footer />
